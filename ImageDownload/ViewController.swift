0ffdda5f87e1b53f74f5583d0663d82e7b0ff7da
JeongAYoo <jeonga038@naver.com>
2023-03-02 16:22:24 +0900

    Storyboard UI

--- ImageDownload/ViewController.swift
+++ ImageDownload/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var imageView4: UIImageView!
+    @IBOutlet weak var imageView5: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loadImage1Tapped(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func loadImage2Tapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func loadImage3Tapped(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func loadImage4Tapped(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func loadImage5Tapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func loadIAllButtonTapped(_ sender: UIButton) {
+        
+    }
+    
 }
 
